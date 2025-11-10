6e2ba154dd47b366712a27305e856eec58c04b57
Thamindu Rajakaruna <thamindurajakaruna@gmail.com>
2025-11-10 09:06:27 +0530

    [Modified] Enhanced patient model fields with tracking for name, DOB, and gender

--- models/patient.py
+++ models/patient.py
@@ -3,9 +3,15 @@ from odoo import api, fields, models
 
 class HospitalPatient(models.Model): #Creating new class by models.Model
     _name = 'hospital.patient'
+    _inherit = ['mail.thread']
     _description = 'Patient Master'
 
     # adding new field in the hospital.patient (Patient Master) object Model
-    name = fields.Char(string='Patient Name', required=True)
-    date_of_birth = fields.Date(string='DOB')
-    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string='Gender')
+    name = fields.Char(
+        string='Patient Name', required=True, tracking=True
+    )
+    date_of_birth = fields.Date(string='DOB', tracking=True)
+    gender = fields.Selection(
+        [('male', 'Male'), ('female', 'Female')],
+        string='Gender', tracking=True
+    )
