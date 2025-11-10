613715954642d5063dabdd5be1052e2d0ccd4283
Thamindu Rajakaruna <thamindurajakaruna@gmail.com>
2025-11-10 09:08:10 +0530

    [Modified] Updated manifest to include patient readonly views and access control dependencies

--- __manifest__.py
+++ __manifest__.py
@@ -5,8 +5,11 @@
     'author': 'Thamindu Rajakaruna',
     'license': 'LGPL-3',
     'version': '17.0.1.1',
+    'depends': ['mail'],
     'data': [
+        'security/ir.model.access.csv',
         'views/patient_views.xml',
+        'views/patient_readonly_views.xml',
         'views/menu.xml'
     ]
 }
