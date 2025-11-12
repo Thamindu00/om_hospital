fc4a6dcedcc21ea72869f99d3f29c7555ef0ccc0
Thamindu Rajakaruna <thamindurajakaruna@gmail.com>
2025-11-12 16:10:12 +0530

    Add patient_tag_views.xml to manifest for patient tag integration

--- __manifest__.py
+++ __manifest__.py
@@ -11,6 +11,7 @@
         'views/patient_views.xml',
         'views/patient_readonly_views.xml',
         'views/appointment_views.xml',
+        'views/patient_tag_views.xml',
         'views/menu.xml'
     ]
 }
