c6e49e50a65d3b848d886135cfa450bded05c377
Thamindu Rajakaruna <thamindurajakaruna@gmail.com>
2025-11-12 16:04:49 +0530

    [Added] Create PatientTag model with name field for patient identification

---
+++ models/patient_tag.py
@@ -0,0 +1,11 @@
+# Patient's data come here
+from odoo import api, fields, models
+
+class PatientTag(models.Model): #Creating new class by models.Model
+    _name = 'patient.tag'
+    _description = 'Patient Tag'
+
+    # adding new field in the patient.tag (Patient Tag) object Model
+    name = fields.Char(
+        string='Patient Name', required=True, tracking=True
+    )
