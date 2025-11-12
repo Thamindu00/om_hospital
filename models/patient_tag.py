405127263951465175de4bb3538fcf88cdf6df20
Thamindu Rajakaruna <thamindurajakaruna@gmail.com>
2025-11-12 16:26:16 +0530

    Removed tracking option from 'Patient Name' field in PatientTag model because of not inherited the chatter there

--- models/patient_tag.py
+++ models/patient_tag.py
@@ -7,5 +7,5 @@ class PatientTag(models.Model): #Creating new class by models.Model
 
     # adding new field in the patient.tag (Patient Tag) object Model
     name = fields.Char(
-        string='Patient Name', required=True, tracking=True
+         string='Patient Name', required=True,
     )
