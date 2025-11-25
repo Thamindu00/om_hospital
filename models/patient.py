7eedbf94eaaf253bdc40470754c77e1511d44ab1
Thamindu Rajakaruna <thamindurajakaruna@gmail.com>
2025-11-25 16:31:53 +0530

    Implement ondelete decorator to prevent deletion of patients with existing appointments

--- models/patient.py
+++ models/patient.py
@@ -1,5 +1,8 @@
 # Patient's data come here
-from odoo import api, fields, models
+
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError, UserError
+
 
 class HospitalPatient(models.Model): #Creating new class by models.Model
     _name = 'hospital.patient'
@@ -21,3 +24,27 @@ class HospitalPatient(models.Model): #Creating new class by models.Model
         #'patient.tag','patient_tag_rel','patient_id','tag_id',string="Tags" # patient_tag_rel is the relation name
         'patient.tag', string="Tags"
     )
+
+    # Inheriting the delete method which is called as unlink method - super the unlink method
+    # we can do the below logic in same way by using another way that is API ondelete decorator
+    '''
+    def unlink(self):
+        for rec in self:
+            domain = [('patient_id', '=', rec.id)]
+            appointments = self.env['hospital.appointment'].search(domain)
+            if appointments:
+                raise ValidationError(_("You cannot delete the patient now.\nAppointments existing for this patient: %s" % rec.name))
+                # raise UserError(_("You cannot delete the patient now.\nAppointments existing for this patient: %s" % rec.name))
+        # we can perform anything here
+        return super().unlink()
+    '''
+
+    # ondelete decorator in odoo. if any function is defined using this decorator, the function will be executed on deleting the record
+    @api.ondelete(at_uninstall=False)
+    def _check_patient_appointments(self):
+        for rec in self:
+            domain = [('patient_id', '=', rec.id)]
+            appointments = self.env['hospital.appointment'].search(domain)
+            if appointments:
+                raise ValidationError(_("You cannot delete the patient right now." 
+                                        "\nAppointments existing for this patient: %s" % rec.name))
